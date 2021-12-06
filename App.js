750c196ed27ff1e1428ff590a48c2a9fe1042105
sookie-c <92242083+sookie-c@users.noreply.github.com>
2021-12-06 13:16:37 +1030

    Styling city and weather

--- App.js
+++ App.js
@@ -5,8 +5,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.description}>Sunny</Text>
+          <Text style={styles.temp}>7</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +22,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#85C1E9',
+  },
+  city: {
+    flex: 1,
+    marginTop: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: '300',
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: 18,
+    fontWeight: '300',
+    color: 'white',
+  },
+  temp: {
+    fontSize: 120,
+    fontWeight: '200',
+    color: 'white',
   },
 });
